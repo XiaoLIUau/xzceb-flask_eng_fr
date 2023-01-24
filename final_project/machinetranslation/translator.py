aaf832aada3e481d4a54e969088a78082f29a156
XiaoLIUau <xiao.liu.contact@gmail.com>
2023-01-23 23:42:23 -0500

    update coding style

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,7 @@
+import os
 import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,26 +11,26 @@ url = os.environ['url']
 
 authenticator = IAMAuthenticator('{apikey}')
 language_translator = LanguageTranslatorV3(
-    version='2018-05-01',
-    authenticator=authenticator
+    version = '2018-05-01',
+    authenticator = authenticator
 )
 
 language_translator.set_service_url('{url}')
 
 '''Function english to french'''
 def englishToFrench(englishText):
-    #write the code here
+    '''Function take english text and translate to french'''
     translation = language_translator.translate(
-    text=englishText,
-    model_id='en-fr').get_result()
-    frenchText=translation.get('translations')[0].get('translation')
+    text = englishText,
+    model_id = 'en-fr').get_result()
+    frenchText = translation.get('translations')[0].get('translation')
     return frenchText
 
 '''Function french to english'''
 def frenchToEnglish(frenchText):
-    #write the code here
+    '''Function take french text and translate to english'''
     translation = language_translator.translate(
-    text=frenchText,
-    model_id='fr-en').get_result()
-    englishText=translation.get('translations')[0].get('translation')
-    return englishText  
+    text = frenchText,
+    model_id = 'fr-en').get_result()
+    englishText = translation.get('translations')[0].get('translation')
+    return englishText
